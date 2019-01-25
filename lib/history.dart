ddf1d58c12cf5820ae9235e74a8ae9e4cee165ba
jaap aarts <jaap.aarts1@gmail.com>
2019-01-25 14:11:15 +0100

    changed history

--- lib/history.dart
+++ lib/history.dart
@@ -6,54 +6,49 @@ import 'package:duration/duration.dart';
 import 'authentication.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import 'localizations.dart';
-
 class HistoryPage extends StatelessWidget {
   static MiniMapSheet map = MiniMapSheet();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(AppLocalizations.of(context).helpHistoryTitle),
-        ),
-        body: ListView(
-          children: <Widget>[
-            StreamBuilder(
-              stream: Firestore.instance
-                  .collection("tickets")
-                  .where("user", isEqualTo: userState.getID())
-                  .orderBy("start", descending: true)
-                  .snapshots(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                    return Text('Loading...');
-                  default:
-                    return Column(
-                      children: snapshot.data.documents
-                          .map((DocumentSnapshot ticket) {
-                        return ParkingLocation(
-                            LatLng(ticket['location'].latitude,
-                                ticket['location'].longitude),
-                            DateFormat.yMMMd()
-                                .format(ticket['start'])
-                                .toString(),
-                            ticket['licencePlate'].toString(),
-                            (ticket['price'] / 100)
-                                .toStringAsFixed(2)
-                                .toString(),
-                            ticket['end'].difference(ticket['start']),
-                            map);
-                      }).toList(),
-                    );
-                }
-              },
-            )
-          ],
-        ));
+      appBar: AppBar(
+        title: Text("Parking History"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          StreamBuilder(
+            stream: Firestore.instance
+                .collection("tickets")
+                .where("user", isEqualTo: userState.getID())
+                .orderBy("start", descending: true)
+                .snapshots(),
+            builder: (BuildContext context,
+                AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError)
+                return Text('Error: ${snapshot.error}');
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return Text('Loading...');
+                default:
+                  return Column(
+                    children: snapshot.data.documents
+                        .map((DocumentSnapshot ticket) {
+                      return ParkingLocation(
+                        LatLng(ticket['location'].latitude, ticket['location'].longitude),
+                        DateFormat.yMMMd().format(ticket['start']).toString(),
+                        ticket['licencePlate'].toString(),
+                        (ticket['price'] / 100).toStringAsFixed(2).toString(),
+                        ticket['end'].difference(ticket['start']),
+                        map
+                      );
+                    }).toList(),
+                  );
+              }
+            },
+          )
+        ],
+      ));
   }
 }
 
@@ -65,33 +60,40 @@ class ParkingLocation extends ListTile {
   final Duration duration;
   final MiniMapSheet mapsheet;
 
-  ParkingLocation(this.loc, this.start, this.numberplate, this.price,
-      this.duration, this.mapsheet);
+  ParkingLocation(this.loc, this.start, this.numberplate, this.price, this.duration, this.mapsheet);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        onTap: _showMinMap(context, loc),
-        isThreeLine: true,
-        title: Text("$start (Licence Plate: $numberplate)",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-        subtitle: FutureBuilder(
-            future: getAdressFromLocation(loc.latitude, loc.longitude),
-            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-              if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-              switch (snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  return Text('Loading...');
-                default:
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text("Close to ${snapshot.data.toString()}"),
-                      Text("Price: €$price (${_getTimeSpend()})")
-                    ],
-                  );
-              }
-            }));
+      onTap: _showMinMap(context, loc),
+      isThreeLine: true,
+      title: Text(
+        "$start (Licence Plate: $numberplate)",
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+      subtitle: FutureBuilder(
+        future: getAdressFromLocation(
+            loc.latitude,
+            loc.longitude),
+        builder: (BuildContext context,
+            AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasError)
+            return Text('Error: ${snapshot.error}');
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Text('Loading...');
+            default:
+              return Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Close to ${snapshot.data.toString()}"),
+                  Text(
+                      "Price: €$price (${_getTimeSpend()})")
+                ],
+              );
+          }
+        })
+    );
   }
 
   getAdressFromLocation(double latitude, double longitude) {
@@ -106,21 +108,22 @@ class ParkingLocation extends ListTile {
     });
   }
 
-  _getTimeSpend() {
+  _getTimeSpend(){
     return printDuration(duration);
   }
 
-  Function _showMinMap(BuildContext context, LatLng loc) {
-    return () {
+  Function _showMinMap(BuildContext context, LatLng loc){
+    return (){
       showModalBottomSheet<void>(
-          context: context,
-          builder: (BuildContext context) {
-            return mapsheet;
-          });
+        context: context,
+        builder: (BuildContext context) {
+          return mapsheet;
+        });
       mapsheet.setLocation(loc);
     };
   }
 }
+
 
 class MiniMapSheet extends StatelessWidget {
   static GoogleMapController mapController;
@@ -130,13 +133,10 @@ class MiniMapSheet extends StatelessWidget {
   MiniMapSheet() {
     print("init new map");
     mapsView = Padding(
-        padding: const EdgeInsets.all(0.0),
-        child: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(0, 0),
-          ),
-        ));
+      padding: const EdgeInsets.all(0.0),
+      child: GoogleMap(
+        onMapCreated: _onMapCreated,
+      ));
   }
 
   @override
@@ -144,11 +144,10 @@ class MiniMapSheet extends StatelessWidget {
     print("building now the minimapsheet");
     return mapsView;
   }
-
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
-    if (loc != null) {
-      mapController.moveCamera(CameraUpdate.newCameraPosition(
+    if (loc != null){
+     mapController.moveCamera(CameraUpdate.newCameraPosition(
         CameraPosition(
           bearing: 0.00,
           target: loc,
@@ -157,25 +156,23 @@ class MiniMapSheet extends StatelessWidget {
         ),
       ));
     }
-
     print("initiated maps view");
   }
-
-  setLocation(LatLng target) {
+  setLocation(LatLng target){
     mapController == null
-        ? loc = target
-        : () {
-            mapController
-                .moveCamera(CameraUpdate.newCameraPosition(CameraPosition(
-              bearing: 0.00,
-              target: target,
-              tilt: 0,
-              zoom: 18.0,
-            )));
-            mapController.addMarker(MarkerOptions(
-              draggable: false,
-              position: target,
-            ));
-          }();
+      ? loc = target
+      :(){
+        mapController.moveCamera(CameraUpdate.newCameraPosition(
+          CameraPosition(
+            bearing: 0.00,
+            target: target,
+            tilt: 0,
+            zoom: 18.0,
+          )));
+        mapController.addMarker(MarkerOptions(
+          draggable: false,
+          position: target,
+        ));
+      }();
   }
 }
